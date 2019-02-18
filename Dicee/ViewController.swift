9c1557dfea75c1f6dcc3fd16f9af4e1b53e4c153
Mpho Nkuna <mcnkuna@addcolour.co.za>
2019-02-18 11:09:03 +0200

    Function for rolling dice

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,14 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var diceImageView1: UIImageView!
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     
+    @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rollDice()
     }
     
     @IBAction func rollDiceButtonPressed(_ sender: UIButton) {
+         rollDice()
+    }
+    
+    func rollDice() {
+        
+        diceImageView1.image = UIImage(named: diceArray[Int(arc4random_uniform(6))])
+        diceImageView2.image = UIImage(named: diceArray[Int(arc4random_uniform(6))])
     }
 }
